52bd3a7c62cb0d107e6d49f26a3a3708381615e7
DGE - Titan3070 <104428173+Titan3070@users.noreply.github.com>
2022-10-20 17:47:28 +0200

    cardsFinish

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,6 +37,23 @@ module.exports = {
          pink: colors.pink,
          rose: colors.rose,
       },
+      screens: {
+         sm: "420px",
+         // => @media (min-width: 420px) { ... }
+
+         md: "640px",
+         // => @media (min-width: 768px) { ... }
+
+         lg: "768px",
+         // => @media (min-width: 1024px) { ... }
+
+         xl: "1024px",
+         // => @media (min-width: 1280px) { ... }
+
+         "2xl": "1280px",
+         // => @media (min-width: 1536px) { ... }
+      },
+
       extend: {},
    },
    plugins: [],
