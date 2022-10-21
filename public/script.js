b5cfba12699e3bb9ea2623512ca3bb892a9f04e0
DGE - Titan3070 <104428173+Titan3070@users.noreply.github.com>
2022-10-21 13:07:07 +0200

    breake

--- public/script.js
+++ public/script.js
@@ -1,5 +1,33 @@
 "use strict";
-function addNumbers(x, y) {
-    return x + y;
+// global variable who need in the project for all and rendering!
+let url = "https://pokeapi.co/api/v2/pokemon/"; //poke api v2 url
+let currentPokemonAsJson;
+let found = true; //found a other pokemon?
+//to fetch poke api values
+async function getPokemonValueByApi(x) {
+    let response = await fetch(url + x.toString());
+    if (response.status == 404) {
+        found = false;
+    }
+    return (currentPokemonAsJson = await response.json());
 }
-console.log(addNumbers(3, 6));
+//greated data values we need for the current pokmon from the api
+async function greatCurrentPokemonValuesFromApi() {
+    for (let i = 905; found === true; i++) {
+        await getPokemonValueByApi(i);
+        if (currentPokemonAsJson != null) {
+            let pokemonId = parseInt(currentPokemonAsJson["id"]);
+            let pokemonName = currentPokemonAsJson["name"];
+            let pokemonImage = currentPokemonAsJson["sprites"]["front_shiny"];
+            found = true;
+            console.log(i);
+        }
+        else {
+            found = false;
+        }
+    }
+}
+//go start rendering
+async function init() {
+    greatCurrentPokemonValuesFromApi();
+}
--- ts/script.ts
+++ ts/script.ts
@@ -1,4 +1,34 @@
-function addNumbers(x: number, y: number) {
-   return x + y;
+// global variable who need in the project for all and rendering!
+let url = "https://pokeapi.co/api/v2/pokemon/"; //poke api v2 url
+let currentPokemonAsJson: any;
+let found: boolean = true; //found a other pokemon?
+
+//to fetch poke api values
+async function getPokemonValueByApi(x: number) {
+   let response = await fetch(url + x.toString()).catch((found = false));
+
+   return (currentPokemonAsJson = await response.json());
 }
-console.log(addNumbers(3, 6));
+
+//greated data values we need for the current pokmon from the api
+async function greatCurrentPokemonValuesFromApi() {
+   for (let i = 905; found === true; i++) {
+      await getPokemonValueByApi(i);
+
+      if (currentPokemonAsJson != null) {
+         let pokemonId: number = parseInt(currentPokemonAsJson["id"]);
+         let pokemonName: string = currentPokemonAsJson["name"];
+         let pokemonImage: string =
+            currentPokemonAsJson["sprites"]["front_shiny"];
+         found = true;
+         console.log(i);
+      } else {
+         found = false;
+      }
+   }
+}
+
+//go start rendering
+async function init() {
+   greatCurrentPokemonValuesFromApi();
+}
