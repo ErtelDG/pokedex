8cc768b53faf5e970e155ca462e12be9d422e77f
DGE - Titan3070 <104428173+Titan3070@users.noreply.github.com>
2022-10-27 18:37:29 +0200

    makeCssSmaller

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,59 +2,67 @@ const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   content: [],
-   theme: {
-      colors: {
-         transparent: "transparent",
-         current: "currentColor",
-         black: colors.black,
-         white: colors.white,
-         gray: colors.slate,
-         green: colors.emerald,
-         purple: colors.violet,
-         yellow: colors.amber,
-         pink: colors.fuchsia,
-         slate: colors.slate,
-         gray: colors.gray,
-         zinc: colors.zinc,
-         neutral: colors.neutral,
-         stone: colors.stone,
-         red: colors.red,
-         orange: colors.orange,
-         amber: colors.amber,
-         yellow: colors.yellow,
-         lime: colors.lime,
-         green: colors.green,
-         emerald: colors.emerald,
-         teal: colors.teal,
-         cyan: colors.cyan,
-         sky: colors.sky,
-         blue: colors.blue,
-         indigo: colors.indigo,
-         violet: colors.violet,
-         purple: colors.purple,
-         fuchsia: colors.fuchsia,
-         pink: colors.pink,
-         rose: colors.rose,
+   purge: {
+      // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+      enabled: process.env.NODE_ENV === "production",
+      content: [
+         "public/index.html",
+         "public/templates/pokemon_card/pokemon_card.html",
+         "public/js/*.js",
+      ],
+      theme: {
+         colors: {
+            transparent: "transparent",
+            current: "currentColor",
+            black: colors.black,
+            white: colors.white,
+            gray: colors.slate,
+            green: colors.emerald,
+            purple: colors.violet,
+            yellow: colors.amber,
+            pink: colors.fuchsia,
+            slate: colors.slate,
+            gray: colors.gray,
+            zinc: colors.zinc,
+            neutral: colors.neutral,
+            stone: colors.stone,
+            red: colors.red,
+            orange: colors.orange,
+            amber: colors.amber,
+            yellow: colors.yellow,
+            lime: colors.lime,
+            green: colors.green,
+            emerald: colors.emerald,
+            teal: colors.teal,
+            cyan: colors.cyan,
+            sky: colors.sky,
+            blue: colors.blue,
+            indigo: colors.indigo,
+            violet: colors.violet,
+            purple: colors.purple,
+            fuchsia: colors.fuchsia,
+            pink: colors.pink,
+            rose: colors.rose,
+         },
+         screens: {
+            sm: "420px",
+            // => @media (min-width: 420px) { ... }
+
+            md: "640px",
+            // => @media (min-width: 768px) { ... }
+
+            lg: "768px",
+            // => @media (min-width: 1024px) { ... }
+
+            xl: "1024px",
+            // => @media (min-width: 1280px) { ... }
+
+            "2xl": "1440px",
+            // => @media (min-width: 1536px) { ... }
+         },
+
+         extend: {},
       },
-      screens: {
-         sm: "420px",
-         // => @media (min-width: 420px) { ... }
-
-         md: "640px",
-         // => @media (min-width: 768px) { ... }
-
-         lg: "768px",
-         // => @media (min-width: 1024px) { ... }
-
-         xl: "1024px",
-         // => @media (min-width: 1280px) { ... }
-
-         "2xl": "1440px",
-         // => @media (min-width: 1536px) { ... }
-      },
-
-      extend: {},
+      plugins: [],
    },
-   plugins: [],
 };
